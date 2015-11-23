604372c51e33c041ad1af967ff7910b8497dccdd
deakpal <attila.deakpal@chilisoft.org>
2015-11-23 18:58:18 +0100

    promises 2

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -55,7 +55,7 @@ $(function() {
                 },
                 error: function(response) {
                     // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success').html("<div class='alert alert-danger '>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>"+response+"</strong>");
--- controllers/global.js
+++ controllers/global.js
@@ -6,7 +6,6 @@
 	var saveFormData = function(app, type, params){
 		
 		var saveData = function(done){
-			console.log(params);
 				var newFormData = app.models.formData({
 					type: type,
 					name: params.name,
@@ -14,15 +13,16 @@
 					email: params.email,
 					message: params.message
 				}).save(function(err) {
-					console.log("save");
+					console.log('DB:');
 					if (err) {
 						throw err;	
 						console.log(err);
 						done(err);
-					}
-					console.log('FormData created!');
-					done(null,'FormData created!');
-				}).then(done(null,'FormData created!'));	
+					} else {
+						console.log('FormData created!');
+						done(null,'FormData created!');
+					} 
+				})	
 		};
 		return Q.nfcall(saveData);
 	};
@@ -30,8 +30,12 @@
 	var sendMail = function(app,type,params) {
 		
 		var send = function(done) {
-			console.log('mail');
 			var mailString = "Típus: "+type+"\n"+"Név: "+params.name+ "\n" + "Telefonszám: "+params.phone+"\n"+"Email cím: "+params.email+" \n"+ "Üzenet: "+params.message;
+			console.log('///////////////////');
+			console.log(mailString);
+			console.log('///////////////////');
+			console.log('');
+			console.log('mail:');
 			app.services.mail.sendMail({
 				to: config.user,
 				subject: 'Kapcsolatfelvétel',
@@ -43,7 +47,6 @@
 					console.log('--------------------');
 					done(error);
 				} else {
-					console.log(mailString);
 					console.log('Message sent: ' + info.response);
 					done(null,'sent');	
 				}
@@ -56,27 +59,22 @@
 		async.waterfall([
 				function(doneA) {
 					sendMail(app,package,req.body).then(function(response) {
-							console.log('doneA elott');
 							doneA(null, response);
 						})
 						.catch(doneA);
 				},
 				function(response, doneB) {
 					saveFormData(app,package,req.body).then(function(response) {
-							console.log('doneB elott');
 							doneB(null, response);
 						})
 						.catch(doneB);
 				}
 			], function(err) {
 				if (!err) {
-					console.log('waterfall-no-err');
-					console.log("promiseOK");
 					var response = res.__("form.response.success");
 					res.send(res.__("form.response.success"));
 					res.status(200).end(http.STATUS_CODES[200]);
 				} else {
-					console.log('waterfall-err');
 					var response = res.__("form.response.fail");
 					res.send(res.__("form.response.fail"));
 					res.status(500).end(http.STATUS_CODES[500]);
