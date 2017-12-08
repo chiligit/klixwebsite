6b01805bba020257a283b9791b1ef2099dcbf6c3
deakpal <attila.deakpal@chilisoft.org>
2017-12-08 23:04:40 +0100

    useMongoClient option

--- services/database.js
+++ services/database.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
 	var mongoose = require('mongoose');
-	mongoose.connect(config.mongodbUriString);
+	mongoose.connect(config.mongodbUriString, { useMongoClient: true });
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function callback () {
